47e6b16e5514fc97994e21fcf41ca035709aae73
xiaopihong <260094892@qq.com>
2023-02-08 23:52:47 +0800

    build(deps): 使用vite-plugin-compression进行gzip压缩 \& nginx配置开启gzip压缩功能

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,40 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import StylelintPlugin from 'vite-plugin-stylelint'
-
+import viteCompression from 'vite-plugin-compression'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin(), StylelintPlugin({ fix: true })],
+  plugins: [
+    vue(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      deleteOriginFile: true,
+      // 对于大于1m的文件才进行压缩
+      // threshold:1024000,
+      algorithm: 'gzip',
+      ext: 'gz',
+    }),
+    // #========gzip start
+    // # gzip
+    // # 开启gzip
+    // gzip on;
+    // # 开启gzip静态压缩功能
+    // gzip_static on;
+    // # gzip缓存大小
+    // gzip_buffers 4 16k;
+    // # gzip http版本
+    // gzip_http_version 1.1;
+    // # gzip压缩级别 1-10
+    // gzip_comp_level 5;
+    // # gzip压缩类型
+    // gzip_types text/plain application/javascript text/css application/xml text/javascript application/x-httpd-php image/jpeg image/gif image/png;
+    // # 是否在http header中添加Vary: Accept-Encoding，建议开启
+    // gzip_vary on;
+    // #========gzip end
+    eslintPlugin(),
+    StylelintPlugin({ fix: true }),
+  ],
   server: {
     host: 'localhost',
     port: 9999,
